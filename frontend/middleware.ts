920a0314c062ff8700a4771f4685cb877107404c
SeongChoel <tldtlddl1234@gmail.com>
2025-07-14 13:52:45 +0900

    spring cookie to broswer

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,7 +1,27 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import client from "./src/lib/backend/client";
+import { cookies } from "next/headers";
 
-export function middleware(request: NextRequest) {
-  console.log("middleware");
+export async function middleware(request: NextRequest) {
+  const reqToken = request.cookies.get("accessToken");
+  const nextResponse = NextResponse.next();
+
+  const response = await client.GET("/api/v1/members/me", {
+    headers: {
+      cookie: (await cookies()).toString(),
+    },
+  });
+
+  // console.log("reqToken", reqToken);
+  // console.log(
+  //   "spring server response cookies",
+  //   response.response.headers.get("set-cookie")
+  // );
+
+  const springCookie = response.response.headers.getSetCookie();
+  nextResponse.headers.set("set-cookie", String(springCookie));
+
+  return nextResponse;
 }
 
 export const config = {
